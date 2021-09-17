98e92f4aa1f82a106074e95876ef89fac3fdd675
AxiE1337 <pixell390@gmail.com>
2021-09-17 15:40:09 +0300

    made so chandes and fixes

--- app.js
+++ app.js
@@ -2,15 +2,25 @@ const menu = document.querySelector('#menu')
 const menuBtn = document.querySelector('#header-menu')
 const arrow = document.querySelector('#arrow-btn')
 const container = document.querySelector('.main-container')
+const logo = document.querySelector('.footer-logo')
+const menuBtns = document.querySelector('.menu-buttons')
 
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('open')
 })
 
 arrow.addEventListener('click', () => {
+    let elem = document.querySelector('.main-container')
     window.scrollTo({
-        left: 0,
-        top: 1000,
+        top: elem.offsetTop,
+        behavior: 'smooth',
+    })
+})
+
+logo.addEventListener('click', () => {
+    let elem = document.querySelector('.header')
+    window.scrollTo({
+        top: elem.offsetTop,
         behavior: 'smooth',
     })
 })
