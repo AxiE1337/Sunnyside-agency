08d3395a75fdd5f53e8dd25371af48d26548267e
AxiE1337 <pixell390@gmail.com>
2021-09-17 22:48:42 +0300

    added menu blur

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ const arrow = document.querySelector('#arrow-btn')
 const container = document.querySelector('.main-container')
 const logo = document.querySelector('.footer-logo')
 const menuBtns = document.querySelector('.menu-buttons')
+const divBlur = document.querySelector('#overlay')
 
 menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('open')
+    divBlur.classList.add('blur')
+    menu.classList.add('open')
+    divBlur.addEventListener('click', blurOff)
 })
+
+function blurOff () {
+    setTimeout(() => {
+        divBlur.classList.remove('blur')
+    }, 50) 
+    menu.classList.remove('open')
+}
 
 arrow.addEventListener('click', () => {
     let elem = document.querySelector('.main-container')
